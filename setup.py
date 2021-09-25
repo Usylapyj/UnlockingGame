303d37ee0c11705b9ae45b1602bd2161dbba1304
gliese <novoselovvlad04@gmail.com>
2021-09-25 15:15:05 +0300

    1.0 stable

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
+    author="Usyapyj",
+    author_email='novoselovvlad04@gmail.com',
+    install_requires=['Xlib', 'pygame', 'threading'],
+    python_requires='>=3.6',
     entry_points={
         'console_scripts':
             ['UnlockingGame = UnlockingGame.main:main'],
