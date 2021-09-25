19782e9d835ffa139e74e4896624d51c31ce3534
gliese <novoselovvlad04@gmail.com>
2021-09-25 17:20:27 +0300

    1.1 stable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='UnlockingGame',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     author="Usyapyj",
